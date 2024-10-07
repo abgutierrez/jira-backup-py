1f89beddc12f3d4cee4c973a8d35b35b2318691c
Agustin Barreto <agustinbarreto17@gmail.com>
2024-10-07 10:58:51 -0300

    removed credentials

--- wizard.py
+++ wizard.py
@@ -1,31 +1,34 @@
 import os
+from pathlib import Path
 import json
+from typing import Dict, Any
 
-
-def create_config():
-    jira_host = raw_input("What is your Jira host name? ")
-    user = raw_input("What is your Jira account email address? ")
-    password = raw_input("Paste your Jira API token: ")
-    attachments = raw_input("Do you want to include attachments? (true / false) ")
-    download_locally = raw_input("Do you want to download the backup file locally? (true / false) ")
-    custom_config = {
-        'JIRA_HOST': jira_host,
-        'INCLUDE_ATTACHMENTS': attachments.lower(),
-        'JIRA_EMAIL': user,
-        'API_TOKEN': password,
-        'DOWNLOAD_LOCALLY': download_locally.lower(),
+def create_config() -> None:
+    custom_config: Dict[str, Any] = {
+        'JIRA_HOST': input("¿Cuál es el nombre de tu host de Jira? "),
+        'JIRA_EMAIL': input("¿Cuál es tu dirección de correo electrónico de la cuenta Jira? "),
+        'API_TOKEN': input("Pega tu token API de Jira: "),
+        'INCLUDE_ATTACHMENTS': input("¿Quieres incluir archivos adjuntos? (true / false) ").lower(),
+        'DOWNLOAD_LOCALLY': input("¿Quieres descargar el archivo de respaldo localmente? (true / false) ").lower(),
         'UPLOAD_TO_S3': {
             'S3_BUCKET': "",
             'AWS_ACCESS_KEY': "",
             'AWS_SECRET_KEY': ""
         }
     }
-    upload_backup = raw_input("Do you want to upload the backup file to S3? (true / false) ")
-    if upload_backup.lower() == 'true':
-        custom_config['UPLOAD_TO_S3']['S3_BUCKET'] = raw_input("What is the S3 bucket name? ")
-        custom_config['UPLOAD_TO_S3']['AWS_ACCESS_KEY'] = raw_input("What is your AWS access key? ")
-        custom_config['UPLOAD_TO_S3']['AWS_SECRET_KEY'] = raw_input("What is your AWS secret key? ")
 
-    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
-    with open(config_path, 'w+') as config_file:
-        json.dump(custom_config, config_file)
+    if input("¿Quieres subir el archivo de respaldo a S3? (true / false) ").lower() == 'true':
+        custom_config['UPLOAD_TO_S3'].update({
+            'S3_BUCKET': input("¿Cuál es el nombre del bucket S3? "),
+            'AWS_ACCESS_KEY': input("¿Cuál es tu clave de acceso AWS? "),
+            'AWS_SECRET_KEY': input("¿Cuál es tu clave secreta AWS? ")
+        })
+
+    config_path = Path(__file__).parent / 'config.json'
+    with config_path.open('w') as config_file:
+        json.dump(custom_config, config_file, indent=4)
+
+    print(f"Configuración guardada en {config_path}")
+
+if __name__ == "__main__":
+    create_config()
